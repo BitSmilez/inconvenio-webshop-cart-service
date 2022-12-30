2379a7894e52c68b1f0d81684573d4de5d4feb8a
jutnh <jutnhbr@gmail.com>
2022-12-30 18:50:03 +0100

    INCONV-8 [/] Formatting

--- src/main/java/com/bitsmilez/cartmicroservice/CartMicroserviceApplication.java
+++ src/main/java/com/bitsmilez/cartmicroservice/CartMicroserviceApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class CartMicroserviceApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(CartMicroserviceApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(CartMicroserviceApplication.class, args);
+    }
 
 }
