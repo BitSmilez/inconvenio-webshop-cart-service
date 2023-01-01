95b58c35eb514c1ba872de095c6242a93a7eae65
Biri <test@email>
2023-01-01 19:33:12 +0100

    INCONV-21 [+] toString and constructor

--- src/main/java/com/bitsmilez/cartmicroservice/core/domain/model/Cart.java
+++ src/main/java/com/bitsmilez/cartmicroservice/core/domain/model/Cart.java
@@ -1,6 +1,7 @@
 package com.bitsmilez.cartmicroservice.core.domain.model;
 
 
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import jakarta.persistence.OneToMany;
@@ -8,19 +9,32 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import java.util.Set;
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
+
 public class Cart {
     @Id
     private String cartID;
-    @OneToMany(mappedBy="cart")
-    private Set<Product> items;
+
+    @OneToMany(cascade = CascadeType.ALL,
+
+            mappedBy = "cart")
+    private List<Product> items = new ArrayList<>();
 
     public Cart(String cartID) {
-        this.cartID=cartID;
+        this.cartID = cartID;
+    }
+
+    @Override
+    public String toString() {
+        return "Cart{" +
+                "cartID='" + cartID + '\'' +
+                ", items=" +items.size() +
+                '}';
     }
 }
