f12b4257c331facb9dfcd90c455943517441a81c
jutnh <jutnhbr@gmail.com>
2023-01-02 01:54:34 +0100

    INCONV-8 [/] Parsed Strings to UUID

--- src/main/java/com/bitsmilez/cartmicroservice/port/mapper/Mapper.java
+++ src/main/java/com/bitsmilez/cartmicroservice/port/mapper/Mapper.java
@@ -8,35 +8,38 @@ import com.bitsmilez.cartmicroservice.port.dto.ProductDTO;
 import org.modelmapper.ModelMapper;
 
 import java.util.ArrayList;
+import java.util.UUID;
 
 public class Mapper {
-    public static ProductDTO toProductDTO(Product product){
+    public static ProductDTO toProductDTO(Product product) {
 
-        ProductDTO dto = new ModelMapper().map(product,ProductDTO.class);
+        ProductDTO dto = new ModelMapper().map(product, ProductDTO.class);
         dto.setProductID(product.getProductID().getProductID());
         return dto;
     }
-    public static ProductDTO toProductDTO(ProductMessage message){
+
+    public static ProductDTO toProductDTO(ProductMessage message) {
         CartDTO cart = new CartDTO(message.getUserID());
         cart.setCartID(message.getUserID());
-        return new ProductDTO(message.getProductID(),message.getProductName(),message.getProductPrice(),message.getProductSalesPrice(),message.getProductImg(),message.getQuantity(),cart);
-
-    }
-    public static Product toProduct(ProductDTO product){
-
-        return new ModelMapper().map(product,Product.class);
+        return new ProductDTO(UUID.fromString(message.getProductID()), message.getProductName(), message.getProductPrice(), message.getProductSalesPrice(), message.getProductImg(), message.getQuantity(), cart);
 
     }
 
+    public static Product toProduct(ProductDTO product) {
 
-    public static CartDTO toCartDTO(Cart cart){
-        ArrayList<ProductDTO> products= new ArrayList<>();
-        for (Product product: cart.getItems()) {
+        return new ModelMapper().map(product, Product.class);
+
+    }
+
+
+    public static CartDTO toCartDTO(Cart cart) {
+        ArrayList<ProductDTO> products = new ArrayList<>();
+        for (Product product : cart.getItems()) {
             products.add(toProductDTO(product));
         }
 
 
-        CartDTO dto =  new ModelMapper().map(cart, CartDTO.class);
+        CartDTO dto = new ModelMapper().map(cart, CartDTO.class);
         dto.setItems(products);
         dto.calculateTotal();
         return dto;
@@ -44,9 +47,9 @@ public class Mapper {
 
     }
 
-    public static Cart toCart(CartDTO cart){
-        ArrayList<Product> products= new ArrayList<>();
-        for (ProductDTO product: cart.getItems()) {
+    public static Cart toCart(CartDTO cart) {
+        ArrayList<Product> products = new ArrayList<>();
+        for (ProductDTO product : cart.getItems()) {
             products.add(toProduct(product));
         }
         Cart entity = new ModelMapper().map(cart, Cart.class);
