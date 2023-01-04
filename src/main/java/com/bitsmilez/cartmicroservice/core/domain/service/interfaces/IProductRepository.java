1a06d82041c48eccb124cdc6203665956ffb0510
Biri <test@email>
2023-01-05 00:25:03 +0100

    INCONV-8 [+] added search

--- src/main/java/com/bitsmilez/cartmicroservice/core/domain/service/interfaces/IProductRepository.java
+++ src/main/java/com/bitsmilez/cartmicroservice/core/domain/service/interfaces/IProductRepository.java
@@ -1,10 +1,16 @@
 package com.bitsmilez.cartmicroservice.core.domain.service.interfaces;
 
 import com.bitsmilez.cartmicroservice.core.domain.model.Product;
+import com.bitsmilez.cartmicroservice.core.domain.model.ProductID;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.UUID;
 @Repository
-public interface IProductRepository extends JpaRepository<Product, UUID> {
+public interface IProductRepository extends JpaRepository<Product, ProductID> {
+
+    ArrayList<Product> findByProductIDCartID(String cartID);
+
 }
