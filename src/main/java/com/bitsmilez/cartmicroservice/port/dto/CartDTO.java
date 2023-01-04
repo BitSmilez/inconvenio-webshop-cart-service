71e13ffccfb5a43d98b863abc0c31db59ab4004a
jutnh <jutnhbr@gmail.com>
2023-01-04 23:14:33 +0100

    INCONV-8 [/] Formatting / Removed unused imports

--- src/main/java/com/bitsmilez/cartmicroservice/port/dto/CartDTO.java
+++ src/main/java/com/bitsmilez/cartmicroservice/port/dto/CartDTO.java
@@ -1,14 +1,11 @@
 package com.bitsmilez.cartmicroservice.port.dto;
 
-import com.bitsmilez.cartmicroservice.core.domain.model.Product;
-import lombok.AllArgsConstructor;
 import lombok.Data;
-import lombok.NoArgsConstructor;
 
 import java.math.BigDecimal;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Set;
+
 
 @Data
 
@@ -19,10 +16,11 @@ public class CartDTO {
     private BigDecimal total;
 
     public CartDTO(String cartID) {
-        this.cartID=cartID;
+        this.cartID = cartID;
 
     }
-    public void addItem(ProductDTO product){
+
+    public void addItem(ProductDTO product) {
         items.add(product);
         product.setCart(this);
         calculateTotal();
@@ -31,17 +29,17 @@ public class CartDTO {
     public CartDTO(String cartID, List<ProductDTO> items) {
         this.cartID = cartID;
         this.items = items;
-         calculateTotal();
+        calculateTotal();
     }
 
     public CartDTO() {
     }
 
-    public void calculateTotal (){
+    public void calculateTotal() {
         BigDecimal total = new BigDecimal("0.0");
-        for (ProductDTO product: items) {
-            BigDecimal price = product.getProductSalesPrice()!= null ? product.getProductSalesPrice() : product.getProductPrice();
-            total =  total.add(price.multiply(BigDecimal.valueOf(product.getQuantity())));
+        for (ProductDTO product : items) {
+            BigDecimal price = product.getProductSalesPrice() != null ? product.getProductSalesPrice() : product.getProductPrice();
+            total = total.add(price.multiply(BigDecimal.valueOf(product.getQuantity())));
         }
         this.total = total;
     }
@@ -51,7 +49,7 @@ public class CartDTO {
     public String toString() {
         return "CartDTO{" +
                 "cartID='" + cartID + '\'' +
-                ", items=" +items.size() +
+                ", items=" + items.size() +
                 '}';
     }
 }
