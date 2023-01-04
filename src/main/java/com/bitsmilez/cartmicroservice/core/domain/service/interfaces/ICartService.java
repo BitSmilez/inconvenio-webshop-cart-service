f968031546f7484593ffa4bb31592c832647ca35
jutnh <jutnhbr@gmail.com>
2023-01-04 22:30:12 +0100

    INCONV-8 [/] Changed addProduct parameters to ProductDTO

--- src/main/java/com/bitsmilez/cartmicroservice/core/domain/service/interfaces/ICartService.java
+++ src/main/java/com/bitsmilez/cartmicroservice/core/domain/service/interfaces/ICartService.java
@@ -1,14 +1,11 @@
 package com.bitsmilez.cartmicroservice.core.domain.service.interfaces;
 
 import com.bitsmilez.cartmicroservice.port.dto.CartDTO;
-
-import java.math.BigDecimal;
-import java.util.UUID;
-
+import com.bitsmilez.cartmicroservice.port.dto.ProductDTO;
 
 public interface ICartService {
 
 
     CartDTO getProducts(String cartID);
-    void addProduct(String cartID, UUID productID, String productName, BigDecimal productPrice, BigDecimal productSalesPrice, String productImg, int quantity);
+    void addProduct(ProductDTO product);
 }
