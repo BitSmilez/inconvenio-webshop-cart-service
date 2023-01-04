6d7d190de3cf62ec205709db6471e90d3f62b20f
jutnh <jutnhbr@gmail.com>
2023-01-04 23:15:34 +0100

    INCONV-8 [-] Removed unnecessary autowired from main

--- src/main/java/com/bitsmilez/cartmicroservice/CartMicroserviceApplication.java
+++ src/main/java/com/bitsmilez/cartmicroservice/CartMicroserviceApplication.java
@@ -1,6 +1,5 @@
 package com.bitsmilez.cartmicroservice;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
@@ -9,7 +8,8 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 @SpringBootApplication
 @EnableJpaRepositories("com.bitsmilez.cartmicroservice.core.domain.service.interfaces")
 public class CartMicroserviceApplication {
-	@Autowired
+
+
 	public static void main(String[] args) {
 		SpringApplication.run(CartMicroserviceApplication.class, args);
 	}
