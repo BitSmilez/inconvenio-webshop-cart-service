69399835ced53e51d1a180ea1e59b98792e0bc64
jutnh <jutnhbr@gmail.com>
2023-01-06 20:53:13 +0100

    INCONV-8 [-] Removed Unused Imports / Formatting

--- src/main/java/com/bitsmilez/cartmicroservice/core/domain/service/impl/CartServiceImpl.java
+++ src/main/java/com/bitsmilez/cartmicroservice/core/domain/service/impl/CartServiceImpl.java
@@ -1,6 +1,5 @@
 package com.bitsmilez.cartmicroservice.core.domain.service.impl;
 
-import com.bitsmilez.cartmicroservice.core.domain.model.Product;
 import com.bitsmilez.cartmicroservice.core.domain.model.ProductID;
 import com.bitsmilez.cartmicroservice.core.domain.service.interfaces.ICartService;
 import com.bitsmilez.cartmicroservice.core.domain.service.interfaces.IProductRepository;
@@ -10,10 +9,7 @@ import com.bitsmilez.cartmicroservice.port.mapper.Mapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.math.BigDecimal;
-import java.util.ArrayList;
 import java.util.List;
-import java.util.UUID;
 
 @Service
 public class CartServiceImpl implements ICartService {
@@ -30,32 +26,23 @@ public class CartServiceImpl implements ICartService {
 
     @Override
     public CartDTO getProducts(String cartID) {
-
-
         List<ProductDTO> products = productRepository.findByProductIDCartID(cartID).stream().map(Mapper::toProductDTO).toList();
         CartDTO cartDTO = new CartDTO(cartID);
         cartDTO.setItems(products);
         return cartDTO;
-
-
     }
 
     @Override
     public void addProduct(ProductDTO product) {
-        System.out.println("Adding product to cart: " + product);
         ProductID id = new ProductID( product.getProductID(), product.getCartID());
         productRepository.findById(id).ifPresentOrElse(
                 (p) -> {
                     p.setQuantity(p.getQuantity() + product.getQuantity());
                     productRepository.save(p);
                 },
-                () -> {
-                    productRepository.save(Mapper.toProduct(product));
-                }
+                () -> productRepository.save(Mapper.toProduct(product))
         );
-
     }
-
 }
 
 
