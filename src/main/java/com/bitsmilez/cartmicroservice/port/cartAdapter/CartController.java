aac5edb4c9ab3bedba463c58295cd7d3dda684fe
Biri <test@email>
2023-01-06 22:17:50 +0100

    INCONV-21 [+] added better error handeling

--- src/main/java/com/bitsmilez/cartmicroservice/port/cartAdapter/CartController.java
+++ src/main/java/com/bitsmilez/cartmicroservice/port/cartAdapter/CartController.java
@@ -1,8 +1,10 @@
 package com.bitsmilez.cartmicroservice.port.cartAdapter;
 
+import com.bitsmilez.cartmicroservice.core.domain.service.impl.dto.CartDTO;
 import com.bitsmilez.cartmicroservice.core.domain.service.interfaces.ICartService;
-import com.bitsmilez.cartmicroservice.port.dto.CartDTO;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -19,7 +21,14 @@ public class CartController {
     }
 
     @GetMapping(value = "/cart/{cartID}")
-    public CartDTO getProducts(@PathVariable(name = "cartID") String cartID) {
-        return cartService.getProducts(cartID);
+    public ResponseEntity<CartDTO> getProducts(@PathVariable(name = "cartID") String cartID) {
+        CartDTO cart = cartService.getProducts(cartID);
+        if(cart != null){
+            return new ResponseEntity<>(cart, HttpStatus.OK);
+        }
+        else{
+            return new ResponseEntity<>( HttpStatus.NOT_FOUND);
+
+        }
     }
 }
