33c5b9994f4d5ad619f4fdd3b6f6b99129746bd6
Biri <test@email>
2023-01-06 23:27:00 +0100

    INCONV-21 [+] added Cross Origins

--- src/main/java/com/bitsmilez/cartmicroservice/port/cartAdapter/CartController.java
+++ src/main/java/com/bitsmilez/cartmicroservice/port/cartAdapter/CartController.java
@@ -5,10 +5,11 @@ import com.bitsmilez.cartmicroservice.core.domain.service.interfaces.ICartServic
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
-
+@CrossOrigin(origins = "http://localhost:3000")
 @RestController
 public class CartController {
 
