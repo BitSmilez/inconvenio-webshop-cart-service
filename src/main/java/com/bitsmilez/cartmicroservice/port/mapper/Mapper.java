d2b8692dda209ff16e3bbd560ea012e670ae5faa
jutnh <jutnhbr@gmail.com>
2023-02-19 01:19:00 +0100

    INCONV-47 [+] Mapper to generate CheckoutMessage from request

--- src/main/java/com/bitsmilez/cartmicroservice/port/mapper/Mapper.java
+++ src/main/java/com/bitsmilez/cartmicroservice/port/mapper/Mapper.java
@@ -1,10 +1,14 @@
 package com.bitsmilez.cartmicroservice.port.mapper;
 
+import com.bitsmilez.cartmicroservice.config.CheckoutMessage;
 import com.bitsmilez.cartmicroservice.core.domain.model.Product;
 import com.bitsmilez.cartmicroservice.config.ProductMessage;
 import com.bitsmilez.cartmicroservice.core.domain.service.impl.dto.ProductDTO;
+import com.fasterxml.jackson.databind.node.ArrayNode;
+import com.fasterxml.jackson.databind.node.ObjectNode;
 import org.modelmapper.ModelMapper;
 
+import java.util.List;
 import java.util.UUID;
 
 public class Mapper {
@@ -16,21 +20,32 @@ public class Mapper {
     }
 
     public static ProductDTO toProductDTO(ProductMessage message) {
-
-
         return new ProductDTO(UUID.fromString(message.getProductID()), message.getUserID(), message.getProductName(), message.getProductPrice(), message.getProductSalesPrice(), message.getProductImg(), message.getQuantity());
-
     }
 
     public static Product toProduct(ProductDTO product) {
-
         return new ModelMapper().map(product, Product.class);
-
     }
 
+    public static CheckoutMessage toCheckoutMessage(ObjectNode checkoutBody) {
+        ObjectNode orderSummary = (ObjectNode) checkoutBody.get("orderSummary");
+        ArrayNode orderItemsNode = (ArrayNode) orderSummary.get("orderItems");
+        List<String> orderItemIds = orderItemsNode.findValuesAsText("productID");
 
-
-
-
-
+        return new CheckoutMessage(
+                orderSummary.get("userID").asText(),
+                orderSummary.get("address").asText(),
+                orderSummary.get("city").asText(),
+                orderSummary.get("zip").asText(),
+                orderSummary.get("country").asText(),
+                orderSummary.get("paymentMethod").asText(),
+                orderSummary.get("shippingMethod").asText(),
+                orderSummary.get("firstName").asText(),
+                orderSummary.get("lastName").asText(),
+                orderSummary.get("email").asText(),
+                orderSummary.get("phone").asText(),
+                orderSummary.get("orderTotal").decimalValue(),
+                orderItemIds
+        );
+    }
 }
